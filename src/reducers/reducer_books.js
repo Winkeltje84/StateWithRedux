34328d0eadfaeb8c815488c67634dd07febdbc21
Winkeltje84 <bwinkel@gmail.com>
2017-06-13 11:17:34 +0200

    created BooksReducer with data of books / created smart component BookList that links state to BookList props so it can render a list by mapping over it

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'First book of the list'},
+    { title: 'Book #2'},
+    { title: 'Just a third book'},
+    { title: 'Alright, one final book then...'}
+  ]
+}
